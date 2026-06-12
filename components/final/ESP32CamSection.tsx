70a700a452fb0a0d55ab6854ac2b11ab748ef4af
RX5950XT <123271969+RX5950XT@users.noreply.github.com>
2026-06-12 10:35:04 +0800

    docs(final): remove SD quality line from ESP32-CAM section

--- components/final/ESP32CamSection.tsx
+++ components/final/ESP32CamSection.tsx
@@ -24,7 +24,6 @@ export function ESP32CamSection() {
         }
       >
         <p>這是潛艇上的鏡頭實際拍到的畫面。640×480 MJPEG，地面站接收到的就是這個。</p>
-        <p>畫質是 SD，但確實是它眼睛看到的世界。</p>
         <p>拍照與錄影會同步存進 ESP32 裡面的 SD 卡。</p>
       </FinalSection>
     </SlideSection>
